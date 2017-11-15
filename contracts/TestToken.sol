7a35c6cb7563be9f21981ddc9d43782f120050f0
Boris Idzinsky <borisi@techfinancials.com>
2017-11-15 12:50:36 +0200

    edit TestToken

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.4;
 
-import "../token/StandardToken.sol";
+import "/home/administrator/TruffleProjects/node_modules/zeppelin-solidity/contracts/token/BurnableToken.sol";
 
 /**
  * @title TestToken
- * @dev Burnable(can be irreversibly destroyed) ERC20 Token, where all tokens are pre-assigned to the creator.
+ * @dev Burnable, which can be irreversibly destroyed, ERC20 Token, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract TestToken is StandardToken {
+contract TestToken is BurnableToken {
 
 	string public constant name = "TestToken";
 	string public constant symbol = "TST";
@@ -17,14 +17,12 @@ contract TestToken is StandardToken {
 
 	address public owner;
 
-	event Burn(address indexed burner, uint256 value);
-
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
   function TestToken() {
   	totalSupply = INITIAL_SUPPLY;
-  	owner = msg.sender
+  	owner = msg.sender;
     balances[owner] = INITIAL_SUPPLY;
   }
 
@@ -36,20 +34,9 @@ contract TestToken is StandardToken {
     _;
   }
 
-  /**
-   * @dev Burns a specific amount of tokens.
-   * @param _value The amount of token to be burned.
-   */
   function burn(uint256 value) public onlyOwner {
-    require(value > 0);
-    require(value <= balances[msg.sender]);
-    // no need to require value <= totalSupply, since that would imply the
-    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
-
-    address burner = msg.sender;
-    balances[burner] = balances[burner].sub(_value);
-    totalSupply = totalSupply.sub(_value);
-    Burn(burner, value);
-    }
+    //Call BurnableToken.burn(value);
+    super.burn(value);
+  }
 
 }
